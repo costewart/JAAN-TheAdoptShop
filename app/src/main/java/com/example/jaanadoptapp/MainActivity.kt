90636ed19f9b9728650c17dd0d300e612e4e87d5
charlottestewart <costewart4@gmail.com>
2020-04-21 12:12:13 -0700

    format front page, link buttons

--- app/src/main/java/com/example/jaanadoptapp/MainActivity.kt
+++ app/src/main/java/com/example/jaanadoptapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener {
+        dog_page.setOnClickListener {
             startActivity(Intent(this, Dogs::class.java))
         }
     }
